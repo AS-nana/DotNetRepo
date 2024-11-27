fe32e591dd88b2e9c7213954e652a0bbb50c24f8
Adriana Socaciu <adriana.socaciu28@gmail.com>
2024-11-27 18:01:29 +0200

    test continuu

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -8,9 +8,27 @@ namespace TestProject1
         }
 
         [Test]
-        public void Test1()
-        {
-            Console.WriteLine("Insert your name"); ;
+        public void Homework3()
+        {       
+         Console.WriteLine("Insert your First name: ");
+         string firstName = Console.ReadLine();
+         Console.WriteLine("Insert your Last name: ");
+         string lastName = Console.ReadLine();
+         Console.WriteLine("Insert your age: ");
+         Int32 age = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Insert your gender: ");
+         string gender = Console.ReadLine();
+
+         int ageIn20Years = age + 20;
+
+         Console.WriteLine("Your details are as follows: ");
+         Console.WriteLine("Your details are as follows:");
+         Console.WriteLine($"\t- First Name: {firstName}");
+         Console.WriteLine($"\t- Last Name: {lastName}");
+         Console.WriteLine($"\t- Gender: {gender}");
+         Console.WriteLine($"You will be {ageIn20Years} years old in 20 years!");
+
+         
         }
     }
 }
