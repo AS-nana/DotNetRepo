43353931c6589a6fe8f5eaaf54000537865258e4
Adriana Socaciu <adriana.socaciu28@gmail.com>
2024-12-05 11:16:32 +0200

    age in x years

---
+++ VariableWith Test/HWMethods/Persoana.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableWith_Test.HWMethods
+{
+    internal class Persoana
+    {
+
+        public static string fname = "Moga";
+        public static string lname = "Andrei";
+        public static int age = 50;
+        public static char gender = 'M';
+        
+        public static int DisplayAgeAfterX(int x)
+        {
+            var result = age + x;
+            Console.WriteLine($"The user will be {result} after {x} years");
+            return result;
+
+        }
+
+        [Test]
+        public static void DisplayUserDetails()
+        {
+            Console.WriteLine("The person's details are as follows: ");
+            Console.WriteLine("\t- First Name: " + fname);
+            Console.WriteLine("\t- Last Name: " + lname);
+            Console.WriteLine("\t- Gender: " + gender);     
+            DisplayAgeAfterX(12);
+        }
+
+
+
+
+        }
+    }
+
---
+++ VariableWith Test/Methods/MyFirstMethod.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableWith_Test.Methods
+{
+    public class MyFirstMethod
+    {
+
+
+        [Test]
+        public void MetodaTest()
+        {
+           // SumNumbers();
+           // SumNumbers(60, 45);
+           // SumNumbers(60, 45,100);
+
+            int sum = GetResult(15, 20, 66);
+            Console.WriteLine("The result is " + sum);
+        }
+
+        public void SumNumbers() 
+        {
+            int number1 = 10;
+            int number2 = 700;
+            int sum = number1 + number2;
+            Console.WriteLine("The result is " + sum);
+                    
+        }
+
+       public void SumNumbers(int numberA, int numberB)
+        {
+            int sum = numberA + numberB;
+            Console.WriteLine("The result is " + sum);
+        }
+
+
+        public void SumNumbers(int numberA, int numberB, int numberC)
+        {
+            int sum = numberA + numberB + numberC;
+            Console.WriteLine("The result is " + sum);
+        }
+
+        public int GetResult(int numberA, int numberB, int numberC) 
+        {
+            int sum = numberA + numberB + numberC;
+            return sum;
+        }
+
+    }
+}
