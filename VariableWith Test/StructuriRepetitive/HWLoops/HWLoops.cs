6db097a2a95a894ac3ab4b7c8f72ebc8177c588f
Adriana Socaciu <adriana.socaciu28@gmail.com>
2025-01-16 16:46:13 +0200

    skills

---
+++ VariableWith Test/StructuriRepetitive/HWLoops/HWLoops.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using NUnit.Framework;
+using System.Xml.Linq;
+using VariableWith_Test.HWMethods;
+using System.Security.Cryptography.X509Certificates;
+
+namespace VariableWith_Test.StructuriRepetitive.HWLoops
+{
+    /*In the “Person” class add the following:A class attribute “List<string> Skills“ – to store the list of skills for the person.
+     A parameterized method to add a skill to the person.
+    This method also outputs to the console the name of the newly added skill.
+A method to output to console all the skills of the person:
+Use either for or foreach statements to output the list to console.
+    If the currently output skill is “Ninja” DO NOT display it.
+If the currently output skill is “CIA” then exit the loop and output “Classified information, no further skills are displayed!”.*/
+    public class HWLoops
+    {
+        private List<string> Skills = new List<string>
+
+            {
+              "Cooking", "Photography","Programming","Testing","Ninja","Swimming","CIA","Dancing","Singing"
+            };
+        [Test]
+        public void PersonSkills()
+
+        {
+            foreach (string skill in Skills)
+            {
+                Console.WriteLine($"Added a new skill: " + skill);
+            }
+
+            Console.WriteLine("The person has the following skills:");
+            foreach (var skill in Skills)
+            {
+                if (skill == "Ninja")
+                {
+                    // Skip the skill "Ninja"
+                    continue;
+                }
+
+                if (skill == "CIA")
+                {
+                    // Exit the loop when skill is "CIA" and display a message
+                    Console.WriteLine(">>>>>>Classified information, no further skills are displayed!<<<<<");
+                    break;
+                }
+
+                // Output the skill
+
+                Console.WriteLine($"- {skill}");
+            }
+            /*
+                        [Test]
+                        public void DisplaySkillsWithConditions()
+                        {
+                            Console.WriteLine("The person has the following skills:");
+                            foreach (var skill in Skills)
+                            {
+                                if (skill == "Ninja")
+                                {
+                                    // Skip the skill "Ninja"
+                                    continue;
+                                }
+
+                                if (skill == "CIA")
+                                {
+                                    // Exit the loop when skill is "CIA" and display a message
+                                    Console.WriteLine(">>>>>>Classified information, no further skills are displayed!<<<<<");
+                                    break;
+                                }
+
+                                // Output the skill
+
+                                Console.WriteLine($"- {skill}");
+                            }
+                        }*/
+
+
+        }
+    }
+}
---
+++ VariableWith Test/StructuriRepetitive/LoopsArrayLists.cs
@@ -0,0 +1,50 @@
+﻿namespace Loops
+{
+    public class StructuriRepetitive
+    {
+        [Test]
+        public void Test1()
+        {
+            //for (int i= 1; i<=7; i++) ;
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            /*int i = 1;
+
+            for (; i <= 8; i++)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            /*int i = 0;
+            for (; i <= 10;)
+            {
+                Console.WriteLine(i);
+                i++;
+            }*/
+
+            //definim un array
+            string[] numeColegi = new string[3];
+            numeColegi[0] = "Ana";
+            numeColegi[1] = "Maria";
+            numeColegi[2] = "Silviu";
+
+            for (int i = 0; i < numeColegi.Length; i++)
+            {
+                Console.WriteLine(numeColegi[i]);
+            }
+
+            //definim o lista
+            List<string> listaColegi = new List<string>
+            {
+              "Andrei", "Bogdan","Cristi"
+            };
+
+            foreach (string numeColeg in listaColegi)
+            {
+                Console.WriteLine(numeColeg);
+            }
+        }
+    }
+}
